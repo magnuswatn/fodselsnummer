d29856cc9a3a36217cb8124f45440024a5c4f03d
magnuswatn <magnus@watn.no>
2017-01-22 16:03:40 +0100

    Updated setup.py for PyPI release

--- setup.py
+++ setup.py
@@ -1,8 +1,21 @@
-from distutils.core import setup
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
 
 setup(
     name='fodselsnummer',
-    version='1.0',
+    description='For generating and validating of Norwegian "birth numbers" (fodselsnummer)',
+    version='1.1',
     license='MIT',
+    author='Magnus Watn',
+    url='https://github.com/magnuswatn/fodselsnummer',
     py_modules=['fodselsnummer'],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+        ],
 )
